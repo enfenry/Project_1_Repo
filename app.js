edb5fbff12fea84538797a9d7a6fec03f230c2d0
enfenry <enfenry@vt.edu>
2019-02-04 20:58:19 -0500

    separating out data points, some edge cases for different countries

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 $(document).ready(function () {
 
+
     const geocodingAPIkey = 'AIzaSyAVNZ2_elkGOvb8xXsyF5NSS9PQnW_Ze8k';
     const ticketmasterAPIkey = '1FADcqMEkzQiSakwUoKLPibod91GMG6g';
     let geoURL;
@@ -8,17 +9,82 @@ $(document).ready(function () {
     let size = 20;
     let unit = 'miles';
 
+    let DOM = {
+        input: $('#cityInput'),
+        searchButton: $('#searchBtn'),
+    }
 
-    getCoords('Arlington').then(function (coords) {
-        getEvents(coords).then(function (result) {
-            console.log(result);
+
+    DOM.searchButton.on('click', function () {
+        event.preventDefault();
+        let input = DOM.input.val().trim();
+        getCoords(input).then(function (coords) {
+            getEvents(coords).then(function (result) {
+
+                let i = 0;
+
+                const artistName = result._embedded.events[i]._embedded.attractions[0].name;
+
+                const genre = result._embedded.events[i].classifications[0].genre.name;
+                const subGenre = result._embedded.events[i].classifications[0].subGenre.name;
+                const genreSummary = genre + ' / ' + subGenre;
+
+                const venue = result._embedded.events[i]._embedded.venues[0];
+                const venueLocation = createLocationSummary(venue)
+
+                const date = result._embedded.events[i].dates.start.localDate;
+                const time = result._embedded.events[i].dates.start.localTime;
+                const timeZone = result._embedded.events[i].dates.timezone;
+
+                const priceData = result._embedded.events[i].priceRanges[0];
+                const priceSummary = createPriceSummary(priceData);
+
+                const ticketLink = result._embedded.events[i].url;
+
+                console.log(artistName, genreSummary, venueLocation, date, time, timeZone, priceSummary, ticketLink);
+                // console.log(result._embedded);
+
+            });
         });
     });
+
+    function createLocationSummary(venue) {
+        const venueName = venue.name;
+        const venueCity = venue.city.name;
+        const countryCode = venue.country.countryCode;
+        let venueLocation;
+        if (countryCode === 'US') {
+            const venueState = venue.state.stateCode;
+            venueLocation = venueCity + ', ' + venueState;
+        }
+        else {
+            venueLocation =  venueCity + ', ' + countryCode;
+        }
+
+        if (venueName !== undefined) {
+            return venueName + ' - ' + venueLocation;
+        }
+        else {
+            return venueLocation;
+        }
+    }
+
+    function createPriceSummary(priceData) {
+        const min = priceData.min;
+        const max = priceData.max;
+        const currency = priceData.currency;
+        if (min === max) {
+            return 'Price: ' + min + ' ' + currency
+        }
+        else {
+            return 'Price Range: ' + min + ' - ' + max + ' ' + currency
+        }
+    }
 
 
     async function getCoords(userInput) {
         geoURL = "https://maps.googleapis.com/maps/api/geocode/json?address=" + userInput + "&key=" + geocodingAPIkey
-
+        console.log(geoURL);
         const response = await $.ajax({
             url: geoURL,
             method: "GET"
@@ -33,6 +99,7 @@ $(document).ready(function () {
 
     async function getEvents(coords) {
         ticketURL = "https://app.ticketmaster.com/discovery/v2/events.json?latlong=" + coords + "&radius=" + radius + "&unit=" + unit + "&size=" + size + "&classificationName=music&apikey=" + ticketmasterAPIkey;
+        console.log(ticketURL);
 
         const response = await $.ajax({
             type: "GET",
@@ -57,5 +124,12 @@ $(document).ready(function () {
     // TICKETMASTER
     // https://app.ticketmaster.com/discovery/v2/events.json?latlong=38.839787,-77.061339&radius=10&unit=miles&size=200&stateCode=VA&countryCode=US&apikey=1FADcqMEkzQiSakwUoKLPibod91GMG6g
     // https://app.ticketmaster.com/discovery/v2/events.json?latlong=32.735687,-97.10806559999999&radius=20&unit=miles&size=20&classificationName=music&apikey=1FADcqMEkzQiSakwUoKLPibod91GMG6g
+
+    // - TicketMaster
+    // - function to get link to event page
+    // - function to get price range of tickets
+    // - function to get artist
+    // - function to get Venue
+    // - function to get date/time of event
 
 });
